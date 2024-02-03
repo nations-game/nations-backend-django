602b07d0b361f33f051cb48ca2b104f9216d0fa1
haikwu <87882941+haikwu@users.noreply.github.com>
2024-02-02 18:50:14 -0800

    Building Basics

---
+++ nations_backend/models/building.py
@@ -0,0 +1,13 @@
+from django.db.models import (
+    CharField, 
+    ForeignKey, 
+    IntegerField, 
+    Model, 
+    CASCADE
+)
+
+class NationBuilding(Model):
+    nation = ForeignKey("Nation", on_delete=CASCADE, related_name="buildings")
+    building_type = CharField(max_length=100)
+
+    level = IntegerField(default=0)
